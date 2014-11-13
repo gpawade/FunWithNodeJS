b149a43c46da52ea6e44ff991c8f0e5a77087e28
gpawade <gap.subscribe@gmail.com>
2014-11-13 17:03:47 +0530

    fun with nodejs setup

---
+++ FunWithNodeExpress/Services/database.js
@@ -0,0 +1,29 @@
+﻿(function (mdata) {
+    
+    var mongo = require('mongodb');
+    var mongourl = "mongodb://localhost:27017/fundb";
+    var theDb = null;
+    mdata.getdb = function (next) {
+        if (!theDb) {
+            //connect to database
+            mongo.MongoClient.connect(mongourl, function (err, db) {
+                if (err)
+                    next(err, null);
+                else {
+                    theDb = {
+                        db: db,
+                        notes : db.collection("notes")
+                    };
+                    next(null, theDb);
+                }
+            });
+
+        }
+        else {
+            next(null, theDb);
+        }
+
+    };
+
+
+})(module.exports);
---
+++ FunWithNodeExpress/routes/index.js
@@ -0,0 +1,75 @@
+﻿
+/*
+ * GET home page.
+ */
+
+//(function (controller) {
+  
+module.exports = function (app) {
+    var database = require("../Services/database.js");
+    
+    
+    app.get("/", function (req, res) {
+        res.render('index', { title: 'Index ' });
+    });
+    
+    app.get("/api/users", function (req, res) {
+        
+        //res.send([
+        //   {
+        //    name: 'ganesh'
+        //},
+        //{
+        //    name: 'harsh'
+        //}
+        //    ]);
+
+
+        database.getdb(function (err, db) {
+            if (err)
+                console.log("failed:" + err);
+            else {
+                db.notes.find().toArray(function (err, results) {
+                    res.send(results);
+                });
+            }
+        });
+    });
+    
+    //controller.index = function (req, res) {
+    //    res.render('index', { title: 'Index Controller' });
+    //};
+    
+    function seedDatabase() {
+        database.getdb(function (err, db) {
+            if (err)
+                console.log("failed" + err);
+            else {
+                db.notes.count(function (err, count) {
+                    if (err)
+                        console.log("failed to load database count");
+                    else {
+                        if (count == 0) {
+                            console.log("seeding the database...");
+                            var obj = {
+                                name : 'my first collection',
+                                notes : ["one","two","three","four"]
+                            };
+                            db.notes.insert(obj, function (err) {
+                                if (err)
+                                    console.log("failed to insert");
+                            });
+                        }
+                        else {
+                            console.log("database already seeded..");
+                        }
+                    }
+                });
+            }
+        });
+    }    ;
+    
+    seedDatabase();
+};
+//})(module.exports);
+
